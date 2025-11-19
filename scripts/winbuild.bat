277bd1b32d1a3e8c36e61169e8176b53b43ba7e7
cjRem44x <remillardc15@gmail.com>
2025-11-18 20:22:23 -0500

    feat: mod build script

--- scripts/winbuild.bat
+++ scripts/winbuild.bat
@@ -2,26 +2,12 @@
 
 set proj="%cd%/.."
 
-set odin_src=%proj%/src/main/odin
-set rust_src=%proj%/src/main/rust
 set zig_src=%proj%/src/main/zig
-set java_src=%proj%/src/main/java
 set bin=%proj%/bin
-
-@REM cd %java_src%
-@REM javac -d %bin% ^
-@REM     *.java
-@REM java -cp %bin% main.java.Main
-
-@REM cd %odin_src%
-@REM odin run .
-
-@REM cd %rust_src%
-@REM cargo build
-@REM cargo run
 
 cd %zig_src%
 zig build
 zig build run
 
+cd %proj%
 pause
